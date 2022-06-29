6721b1a207d51092c718a652d87bcff3ee96bee5
Manjunath <sreerammanjunath1392@gmail.com>
2022-06-29 15:03:51 +0530

    Business Website

---
+++ js.js
@@ -0,0 +1,9 @@
+function Open() {
+  let Menu = document.getElementById("screen");
+  Menu.style.width = "100vw";
+}
+
+function Close() {
+  let Menu = document.getElementById("screen");
+  Menu.style.width = "0vw";
+}
